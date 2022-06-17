3a8e18acd91a30aee8d015b600058291ec820357
dimaredunlap <dimaredunlap@gmail.com>
2022-06-17 15:53:32 +0000

    exercise complete

--- src/app.js
+++ src/app.js
@@ -6,6 +6,23 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#domain").innerHTML = generateDomain();
+};
+//write your code here
+
+const generateDomain = () => {
+  let pronoun = ["a", "the", "we", "us"];
+  let adjective = ["brave", "charming", "big", "small", "strong", "funny"];
+  let noun = ["dog", "river", "house", "beer", "chocolate", "book"];
+
+  let myArray = [];
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adjective.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        myArray.push("  " + pronoun[i] + adjective[j] + noun[k] + ".com");
+      }
+    }
+  }
+
+  return myArray;
 };
